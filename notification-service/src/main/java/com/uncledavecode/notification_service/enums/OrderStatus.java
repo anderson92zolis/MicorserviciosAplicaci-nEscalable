f8f8006432339244f5e6f40ba39c3ca997ebd0da
anderson92zolis <azolis92@hotmail.es>
2023-09-23 15:10:10 +0200

    finish the topic about kafka

---
+++ notification-service/src/main/java/com/uncledavecode/notification_service/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.uncledavecode.notification_service.enums;
+
+public enum OrderStatus {
+    PLACED,
+    CANCELLED,
+    SHIPPED,
+    DELIVERED
+}
---
+++ orders-service/src/main/java/com/uncledavecode/orders_service/events/OrderEvent.java
@@ -0,0 +1,7 @@
+package com.uncledavecode.orders_service.events;
+
+
+import com.uncledavecode.orders_service.model.enums.OrderStatus;
+
+public record OrderEvent(String orderNumber, int itemsCount, OrderStatus orderStatus) {
+}
